4fef3ef5aaec4b63edde5d875f35a615cc6dddbe
Ariel <99064413+arielwhittingham@users.noreply.github.com>
2023-03-08 01:38:17 -0500

    figuring out the is prime algrithm to speed up my code

--- cpp_practice/exercism/prime_factors.cpp
+++ cpp_practice/exercism/prime_factors.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <functional>
 #include <numeric>
+#include <chrono>
+
 
 /* 
 https://exercism.org/tracks/cpp/exercises/prime-factors/edit
@@ -23,18 +25,33 @@ Note: 12 = 2 × 2 × 3 can also be written using exponents as 12 = 22 × 3
 */
 namespace prime_factors {
 
-    bool is_prime(int& p) {
+//     bool is_prime(int& number) {
 
+//             if (number != 2){
+//         if (number < 2 || number % 2 == 0) {
+//             return false;
+//         }
+//         for(int i=3; (i*i)<=number; i+=2){
+//             if(number % i == 0 ){
+//                 return false;
+//             }
+//         }
+//     }
+//     return true;
+// }
+
+bool is_prime(int& p) {
         int divisors {0};
         
         int middle = (p / 2) + 1;
          for(int n {1}; n <= middle; n++) {
             if(p % n == 0 and n != p) {
                 divisors += 1;
+                if(divisors > 1) {
+                    return false;
+                }
             }
-            if(divisors > 1) {
-                return false;
-            }
+
          }
         if(divisors == 1 and p >= 2) {
              return true;
@@ -50,7 +67,7 @@ namespace prime_factors {
             return 0;
         }
         else {
-            for(int v: vec) {
+            for(int &v: vec) {
                 sum *= v;
              }
         }
@@ -88,7 +105,6 @@ namespace prime_factors {
 
         if(is_prime(n)) {
             ret.push_back(n);
-            std::cout << ret.at(0) << std::endl;
             return ret;
         }
 
@@ -113,11 +129,14 @@ namespace prime_factors {
                     break;
                 }
             }
-            vector_product = prod_vector(ret);
+                if (std::find(possible_primes_up_to_n.begin(), possible_primes_up_to_n.end(), new_n) != possible_primes_up_to_n.end()) {
+                    ret.push_back(new_n);
+                    break;
+            }
         }
 
-        //     sum = std::accumulate(ret.begin(), ret.end(), 1, std::multiplies<int>());
-        //     std::cout << sum << std::endl;
+        // int sum = std::accumulate(ret.begin(), ret.end(), 1, std::multiplies<int>());
+        // std::cout << sum << std::endl;
         // }
         // for(auto x : ret) {
         //     std::cout << x << std::endl;
--- cpp_practice/exercism/test_prime_factors.cpp
+++ cpp_practice/exercism/test_prime_factors.cpp
@@ -1,9 +1,21 @@
 #include "prime_factors.h"
 #include <iostream>
+#include <chrono>
+
 
 int main() {
 
-    prime_factors::of(1);
+    //Start Measuring 
+    auto begin = std::chrono::high_resolution_clock::now();
+    
+    
+    prime_factors::of(901255);
+
+    // Stop measuring time and calculate the elapsed time
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+
+    printf("Time measured: %.3f seconds.\n", elapsed.count() * 1e-9);
     return 0;
 
 }
